a240617fdc8cb450ac2fb81ce784ffd5974f01b0
Tristan Joynes <tajoynes@gmail.com>
2022-06-01 14:16:34 -0500

    Reorganized directory item so styles do not clash

--- src/components/directory-item/directoy-item.component.jsx
+++ src/components/directory-item/directoy-item.component.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 import "./directory-item.styles.scss";
 
 export const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className="directory-item-body">
         <h1>{title}</h1>
-        <Link to="shop">Shop Now</Link>
       </div>
     </div>
   );
